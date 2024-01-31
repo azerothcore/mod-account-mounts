c863d226624c62721d8788bafcc5a043c9e98ff5
blkht01 <25400057+blkht01@users.noreply.github.com>
2024-01-31 23:26:06 +0200

    Modified to allow for excluding certain spells from the module

--- src/mod_account_mount.cpp
+++ src/mod_account_mount.cpp
@@ -5,7 +5,10 @@
 
 class AccountMounts : public PlayerScript
 {
-    static const bool limitrace = false; // This set to true will only learn mounts from chars on the same team, do what you want.
+    static const bool limitrace = true; // This set to true will only learn mounts from chars on the same team, do what you want.
+
+    // Define a list of SpellIDs to exclude from being learned account-wide.
+    std::set<uint32> excludedSpellIds = {470, 578, 6777, 66847, 17464, 18989, 10969, 10796, 35020, 34406}; // Add the SpellIDs you want to exclude here.
 
 public:
     AccountMounts() : PlayerScript("AccountMounts") { }
@@ -32,7 +35,7 @@ public:
                 uint32 race = fields[1].Get<uint8>();
 
                 if ((Player::TeamIdForRace(race) == Player::TeamIdForRace(pPlayer->getRace())) || !limitrace)
-                    Guids.push_back(result1->Fetch()[0].Get<uint32>());
+                    Guids.push_back(fields[0].Get<uint32>());
 
             } while (result1->NextRow());
 
@@ -52,9 +55,13 @@ public:
 
             for (auto& i : Spells)
             {
-                auto sSpell = sSpellStore.LookupEntry(i);
-                if (sSpell->Effect[0] == SPELL_EFFECT_APPLY_AURA && sSpell->EffectApplyAuraName[0] == SPELL_AURA_MOUNTED)
-                    pPlayer->learnSpell(sSpell->Id);
+                // Check if the spell is in the excluded list before learning it
+                if (excludedSpellIds.find(i) == excludedSpellIds.end())
+                {
+                    auto sSpell = sSpellStore.LookupEntry(i);
+                    if (sSpell->Effect[0] == SPELL_EFFECT_APPLY_AURA && sSpell->EffectApplyAuraName[0] == SPELL_AURA_MOUNTED)
+                        pPlayer->learnSpell(sSpell->Id);
+                }
             }
         }
     }
